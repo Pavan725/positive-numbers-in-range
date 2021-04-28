d4c6023e5b51edd0022d12dfd78b8a9aca69d9da
Pavan725 <83358574+Pavan725@users.noreply.github.com>
2021-04-28 21:14:17 +0530

    Create Positive numbers in range.py

---
+++ Positive numbers in range.py
@@ -0,0 +1,10 @@
+list1=[12,7,-5,-14,64]
+print("+ve numbers in list1 ")
+for i in list1:
+    if i>0:
+        print(i)
+print("+ve numbers in list2")
+list2=[12, -95, 14,3]
+for j in list2:
+    if j>0:
+        print(j)
